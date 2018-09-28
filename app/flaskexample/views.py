ec6ee506e925ce99810d71d34424074d41626394
aubin <francois.y.aubin@gmail.com>
2018-09-27 23:44:16 -0400

    removing commented lines.

--- app/flaskexample/views.py
+++ app/flaskexample/views.py
@@ -1,11 +1,6 @@
 from flask import render_template
 from flask import request
 from flaskexample import app
-#from sqlalchemy import create_engine
-#from sqlalchemy_utils import database_exists, create_database
-#import pandas as pd
-#import psycopg2
-#from flaskexample.a_Model import ModelIt
 import running_heaven.code.route_optimizer as route_optimizer
 import running_heaven
 import os
@@ -31,22 +26,15 @@ def output():
     length = float(request.args.get('length'))
     app = route_optimizer.RunRouteOptimizer(show=False)
     d, path = app.run(pt, length)
-    # return render_template("output.html", length_requested=str(length),
 
     f = open(os.path.join(running_heaven_path, 'app/keys/googleApiKey.txt'),
              'r')
     api_key = f.readline()
     f.close()
-    print(api_key)
 
-    #toto = [['40.763480', '-73.967271'],
-    #        ['40.773480', '-73.967271'],
-    #        ['40.763480', '-73.977271']]
-    print(path)
     return render_template("output.html",
                            length_requested='{0:.2f}'.format(length),
                            actual_length='{0:.2f}'.format(d),
-#                           #path_lat_lon=toto,
                            x=path,
                            start_point=pt1,
                            end_point=pt2, length=length,
