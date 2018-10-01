873d2bd7800c4381f0ac6d17d7653b1dbb6a58f9
aubin <francois.y.aubin@gmail.com>
2018-10-01 15:18:38 -0400

    implementing input as intersection instead of longitude and latitude. Improving the style of the web app.

--- app/flaskexample/views.py
+++ app/flaskexample/views.py
@@ -4,6 +4,7 @@ from flaskexample import app
 import running_heaven.code.route_optimizer as route_optimizer
 import running_heaven
 import os
+import googlemaps
 
 
 if '_path' in dir(running_heaven.__path__):
@@ -11,6 +12,14 @@ if '_path' in dir(running_heaven.__path__):
 else:
     running_heaven_path = running_heaven.__path__[0]
 
+
+def get_lon_lat_from_address(gmaps, address):
+    """
+    """
+    geocode_result = gmaps.geocode(address)
+    lon_lat = (geocode_result[0]['geometry']['location']['lng'],
+               geocode_result[0]['geometry']['location']['lat'])
+    return lon_lat
 
 @app.route('/')
 @app.route('/input')
@@ -23,23 +32,40 @@ def about():
 
 @app.route('/output')
 def output():
-    pt1 = request.args.get('pt1')
-    pt2 = request.args.get('pt2')
-    pt = (pt1, pt2)
-    length = float(request.args.get('length'))
-    app = route_optimizer.RunRouteOptimizer(show=False)
-    d, path = app.run(pt, length)
 
+    # load the Google API key for Google Map
     f = open(os.path.join(running_heaven_path, 'app/keys/googleApiKey.txt'),
              'r')
     api_key = f.readline()
     f.close()
 
+    # google map object
+    gmaps = googlemaps.Client(key=api_key)
+
+
+    # starting point
+    pt1_address = request.args.get('pt1')
+    pt1 = get_lon_lat_from_address(gmaps, pt1_address + ', Manhattan, NY')
+    pt1 = '{0:f}_{1:f}'.format(*pt1)
+    # end point
+    pt2_address = request.args.get('pt2')
+    pt2 = get_lon_lat_from_address(gmaps, pt2_address + ', Manhattan, NY')
+    pt2 = '{0:f}_{1:f}'.format(*pt2)
+    # combined points
+    pt = (pt1, pt2)
+    # distance to run
+    length = float(request.args.get('length'))
+
+    # run the optimizer
+    app = route_optimizer.RunRouteOptimizer(show=False)
+    d, path = app.run(pt, length)
+
+    # update the app
     return render_template("output.html",
                            length_requested='{0:.2f}'.format(length),
                            actual_length='{0:.2f}'.format(d),
                            x=path,
-                           start_point=pt1,
-                           end_point=pt2, length=length,
+                           start_point=pt1_address,
+                           end_point=pt2_address, length=length,
                            api_key=api_key)
 
